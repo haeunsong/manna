bc22c031ea2979afc54a68566f7d2ff9410e07de
haeunsong <jamy022576@gmail.com>
2024-03-12 10:15:58 +0900

    프로젝트 세팅

---
+++ manna-back/src/main/java/com/hoya/mannaback/MannaBackApplication.java
@@ -0,0 +1,13 @@
+package com.hoya.mannaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MannaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MannaBackApplication.class, args);
+	}
+
+}
---
+++ manna-back/src/test/java/com/hoya/mannaback/MannaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoya.mannaback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MannaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
