6ddd71e24f0df1bf7a57a601df3e0250df273aaf
haeunsong <jamy022576@gmail.com>
2024-03-13 13:04:44 +0900

    BrowserRouter 설정완료 #6

--- manna-front/src/App.tsx
+++ manna-front/src/App.tsx
@@ -1,25 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import {
+  AUTH_PATH,
+  BOARD_DETAIL_PATH,
+  BOARD_PATH,
+  BOARD_UPDATE_PATH,
+  BOARD_WRITE_PATH,
+  MAIN_PATH,
+  USER_PATH,
+} from "constant";
+import Main from "views/Main";
+import Authentication from "views/Authentication";
+import User from "views/User";
+import BoardWrite from "views/Board/BoardWrite";
+import BoardDetail from "views/Board/BoardDetail";
+import BoardUpdate from "views/Board/BoardUpdate";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path={MAIN_PATH()} element={<Main />} />
+      <Route path={AUTH_PATH()} element={<Authentication />} />
+      <Route path={USER_PATH(":userEmail")} element={<User />} />
+
+      <Route path={BOARD_PATH()}>
+        <Route path={BOARD_WRITE_PATH()} element={<BoardWrite />} />
+        <Route
+          path={BOARD_DETAIL_PATH(":boardNumber")}
+          element={<BoardDetail />}
+        />
+        <Route
+          path={BOARD_UPDATE_PATH(":boardNumber")}
+          element={<BoardUpdate />}
+        />
+      </Route>
+      <Route path="*" element={<h1>404 Not Found</h1>} />
+    </Routes>
   );
 }
 
---
+++ manna-front/src/constant/index.tsx
@@ -0,0 +1,9 @@
+export const MAIN_PATH = () => "/"; // 여기에 카드형식으로 글 목록보임
+export const AUTH_PATH = () => "/auth";
+export const USER_PATH = (userEmail: string) => `/user/${userEmail}`;
+export const BOARD_PATH = () => "/board";
+export const BOARD_DETAIL_PATH = (boardNumber: string | number) =>
+  `detail/${boardNumber}`;
+export const BOARD_WRITE_PATH = () => "write";
+export const BOARD_UPDATE_PATH = (boardNumber: string | number) =>
+  `update/${boardNumber}`;
