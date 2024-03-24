1b67b3e1ae84413fcc63987123471c89110c13f0
haeunsong <jamy022576@gmail.com>
2024-03-24 22:23:55 +0900

    글 작성화면 스크롤 수정 #9

--- manna-front/src/views/Board/BoardWrite/index.tsx
+++ manna-front/src/views/Board/BoardWrite/index.tsx
@@ -1,8 +1,10 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect, ChangeEvent } from "react";
 import "./style.css";
 import useBoardStore from "stores/board.store";
 
 export default function BoardWrite() {
+  // state: 제목 영역 요소 참조 상태
+  const titleRef = useRef<HTMLTextAreaElement | null>(null);
   // state: 본문 영역 요소 참조 상태
   const contentRef = useRef<HTMLTextAreaElement | null>(null);
   // state: 이미지 입력 요소 참조 상태
@@ -18,6 +20,20 @@ export default function BoardWrite() {
   // image box 가 얘를 반복돌면서 출력
   const [imageUrls, setImageUrls] = useState<string[]>([]);
 
+  // event handler
+  const onTitleChangeHandler = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setTitle(e.target.value);
+    if (!titleRef.current) return;
+    titleRef.current.style.height = "auto";
+    titleRef.current.style.height = `${titleRef.current.scrollHeight}px`;
+  };
+  const onContentChangeHandler = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setContent(e.target.value);
+    if (!contentRef.current) return;
+    contentRef.current.style.height = "auto";
+    contentRef.current.style.height = `${contentRef.current.scrollHeight}px`;
+  };
+
   // effect: 마운트시 실행할 함수
   useEffect(() => {
     resetBoard();
@@ -28,12 +44,14 @@ export default function BoardWrite() {
       <div className="board-write-container">
         <div className="board-write-box">
           <div className="board-write-title-box">
-            <input
-              className="board-write-title-input"
-              type="text"
+            <textarea
+              ref={titleRef}
+              className="board-write-title-textarea"
               placeholder="제목을 작성해주세요."
-              value=""
-            ></input>
+              value={title}
+              onChange={onTitleChangeHandler}
+              rows={1}
+            ></textarea>
           </div>
           <div className="divider"></div>
           <div className="board-write-content-box">
@@ -41,6 +59,8 @@ export default function BoardWrite() {
               ref={contentRef}
               className="board-write-content-textarea"
               placeholder="본문을 작성해주세요."
+              value={content}
+              onChange={onContentChangeHandler}
             />
             <div className="icon-button">
               <div className="icon image-box-light-icon"></div>
@@ -56,7 +76,7 @@ export default function BoardWrite() {
             <div className="board-write-image-box">
               <img
                 className="board-write-image"
-                src="https://img.freepik.com/premium-photo/morning-view-of-the-small-town-of-amalfi-on-the-mediterranean-coast-italy_150729-72.jpg?w=1380"
+                src="https://img.freepik.com/free-photo/photo-of-a-canal-of-a-city_1163-159.jpg?w=1380&t=st=1711285772~exp=1711286372~hmac=cfba8d82e0468f4c679fa4f9ddb6f5c3e5afd5db4149da37b730d65e4de8f6eb"
               />
               <div className="icon-button image-close">
                 <div className="icon close-icon"></div>
@@ -65,7 +85,16 @@ export default function BoardWrite() {
             <div className="board-write-image-box">
               <img
                 className="board-write-image"
-                src="https://img.freepik.com/premium-photo/morning-view-of-the-small-town-of-amalfi-on-the-mediterranean-coast-italy_150729-72.jpg?w=1380"
+                src="https://img.freepik.com/premium-photo/modern-city_637484-170.jpg?w=740"
+              />
+              <div className="icon-button image-close">
+                <div className="icon close-icon"></div>
+              </div>
+            </div>
+            <div className="board-write-image-box">
+              <img
+                className="board-write-image"
+                src="https://img.freepik.com/free-photo/beautiful-shot-of-colorful-apartment-buildings-on-a-rocky-hill-on-the-seashore-under-the-blue-sky_181624-7318.jpg?w=1380&t=st=1711285811~exp=1711286411~hmac=7451fd723e618757ca85521fbc4083003f5bbb18d6579e99d54bd9e5895a4d09"
               />
               <div className="icon-button image-close">
                 <div className="icon close-icon"></div>
