29c9ae48a856f37f07b6957989f19717af0f7cc7
haeunsong <jamy022576@gmail.com>
2024-06-12 11:53:56 +0900

    [오류] 토큰이 있어야만 검색이 가능하다. (permit all 을 했음에도 불구하고 안됨) #28

--- manna-back/src/main/java/com/hoya/mannaback/config/SecurityConfig.java
+++ manna-back/src/main/java/com/hoya/mannaback/config/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
                                         try {
                                                 auth.requestMatchers(HttpMethod.GET, "/", "/api/v1/board/list",
                                                                 "/api/v1/board/detail/**", "/file/**",
-                                                                "/api/v1/user/**")
+                                                                "/api/v1/user/**,/api/v1/board/search-list/**")
                                                                 .permitAll()
                                                                 .requestMatchers(HttpMethod.POST, "/api/v1/board/post",
                                                                                 "/api/v1/auth/sign-up",
